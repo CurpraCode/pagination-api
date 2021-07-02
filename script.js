e26aadb1f180f6ea7b92fa79d8cb5db8efee66a1
Oyakoya Curtis <curtsoyaks@gmail.com>
2021-07-02 05:58:42 +0100

    added pagination and styles

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
+
+
+let pages = "1";
+
+// body data response
 const getStock = async (res) => {
-  let urlStock = "https://jsonmock.hackerrank.com/api/stocks?page=1";
+  let urlStock = `https://jsonmock.hackerrank.com/api/stocks?page=${pages}`;
   try {
     const res = await fetch(urlStock);
     const datas= await res.json()
-    console.log(datas.data);
     return await datas.data;
   } catch (error) {
     console.log(error);
   }
 };
 
+// // heading data response
+// const getStockHead = async (res) => {
+//   let urlStock = `https://jsonmock.hackerrank.com/api/stocks?page=${pages}`;
+//   try {
+//     const res = await fetch(urlStock);
+    
+//     console.log(res.json());
+//     return await res.data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+  
+// };
+// getStockHead();
 
+// rendered body data response into html
 const renderStock = async () => {
   let stock = await getStock();
   let stockHtml = "";
@@ -34,3 +53,14 @@ const renderStock = async () => {
   container.innerHTML = stockHtml;
 };
 renderStock();
+
+// onclick button listener for pagination
+document.getElementById("next").addEventListener("click", ()=>{
+  pages++;
+  renderStock()
+})
+document.getElementById("prev").addEventListener("click", ()=>{
+  pages--;
+  renderStock()
+})
+
